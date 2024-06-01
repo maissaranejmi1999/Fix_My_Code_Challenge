2f8fa43d07c8eb9b749be558d5bc57574861f140
maissaranejmi1999 <molinmay1999@gmail.com>
2024-06-01 14:23:05 -0700

    2

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -7,7 +7,7 @@
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+    next if arg !~ /^-?\d+$/
 
     # convert to integer
     i_arg = arg.to_i
@@ -16,11 +16,11 @@ ARGV.each do |arg|
     is_inserted = false
     i = 0
     l = result.size
-    while !is_inserted && i < l do
+    while i < l do
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
